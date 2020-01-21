f73fe2e2b8203e8e9de333d9a4f0f691b7a18916
anass.shaddad@gmail.com <anass.shaddad@gmail.com>
2020-01-21 19:39:08 +0200

    Lesson 01_Start entity Framewor, Basic Operations, Fist VS Single

---
+++ EntityFramework-Demo/EntityFramework-Demo/Controllers/StudentController.cs
@@ -0,0 +1,66 @@
+﻿using EntityFramework_Demo.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EntityFramework_Demo.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            System.Diagnostics.Debugger.Break();
+            //-------------------------------
+            //Create Database
+            var DB = new MyContext();
+            var Students = DB.Students.ToList();
+
+            //-------------------------------
+            //Add Student
+            var Std = new Student() { StudentName = "Anas", Age = 40 };
+            DB.Students.Add(Std);
+
+            var Std2 = new Student() { StudentName = "Mohammad", Age = 30 };
+            DB.Students.Add(Std2);
+
+            var Std3 = new Student() { StudentName = "Bisher", Age = 30 };
+            DB.Students.Add(Std3);
+
+            DB.SaveChanges();
+            //-------------------------------
+            //Update Student
+            Std = DB.Students.Where(x => x.StudentId == 1).FirstOrDefault();
+            Std.StudentName = "Updated";
+            //DB.Students.Update(std); //Wrong
+            DB.SaveChanges();
+
+            //-------------------------------
+            //Remove Student
+            Std = DB.Students.Where(x => x.StudentId == 1).FirstOrDefault();
+            DB.Students.Remove(Std);
+            DB.SaveChanges();
+
+            //-------------------------------
+            //First vs Single
+            Std = DB.Students.Where(x => x.Age == 30).First();
+            Std = DB.Students.First(x => x.Age == 30);
+            try
+            {
+                Std = DB.Students.Single(x => x.Age == 30);
+            }
+            catch (Exception e)
+            {
+
+            }
+            Std = DB.Students.FirstOrDefault(x => x.Age == 20);
+            Std = DB.Students.SingleOrDefault(x => x.Age == 20);
+
+
+            //-------------------------------
+            return View(Students);
+        }
+    }
+}
---
+++ EntityFramework-Demo/EntityFramework-Demo/DAL/MyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EntityFramework_Demo.DAL
+{
+    public class MyContext : DbContext
+    {
+        public MyContext() : base("name=MyConnection")
+        {
+        }
+
+
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+}
